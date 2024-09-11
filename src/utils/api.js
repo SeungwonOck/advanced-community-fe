e932667a19a58053632d485b25cc6140b16d78bb
Chris <100246813+SeungwonOck@users.noreply.github.com>
2024-09-11 21:17:10 +1000

    Proxy Setting

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 // Address may change depends on the condition
-const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
+// const LOCAL_BACKEND = process.env.REACT_APP_LOCAL_BACKEND;
 // const PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND;
-// const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
+const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
 
 const api = axios.create({
-    baseURL: `${LOCAL_BACKEND}/api`,
-//   baseURL: `${PROD_BACKEND}/api`,
+    // baseURL: `${LOCAL_BACKEND}/api`,
+    // baseURL: `${PROD_BACKEND}/api`,
+    baseURL: `${BACKEND_PROXY}/api`,
     headers: {
     "Content-Type": "application/json",
     authorization: `Bearer ${sessionStorage.getItem("token")}`,
